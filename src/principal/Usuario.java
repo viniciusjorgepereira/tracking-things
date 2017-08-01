0b00214de69483e4266f771465b7f028698939c5
Vinicius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2017-08-01 08:23:45 -0300

    Upando projeto inicial

---
+++ src/principal/Usuario.java
@@ -0,0 +1,49 @@
+package principal;
+
+public class Usuario {
+
+	private String nome;
+	private String email;
+	private String telefone;
+	
+	public Usuario(String nome, String email, String telefone) {
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public String toString() {
+		return this.nome + ", " + this.email + ", " + this.telefone;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+}
---
+++ src/principal/UsuarioController.java
@@ -0,0 +1,29 @@
+package principal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioController {
+	
+	private List<Usuario> usuarios;
+
+	public UsuarioController() {
+		this.usuarios = new ArrayList<>();
+	}
+	
+	public void addUsuario() {
+		
+	}
+	
+	public void removeUsuario() {
+		
+	}
+	
+	public void pesquisaUsuario() {
+		
+	}
+	
+	public void attUsuario() {
+		
+	}
+}
