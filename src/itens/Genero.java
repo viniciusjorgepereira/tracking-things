caf02760935c7d1d7d5d5111172b043e431c580f
Vinicius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2017-08-08 09:51:22 -0300

    Adicionado Enum de Genero

---
+++ src/itens/Genero.java
@@ -0,0 +1,30 @@
+package itens;
+
+public enum Genero {
+
+	ACAO("ACAO"),
+	ANIMACAO("ANIMACAO"),
+	AVENTURA("AVENTURA"),
+	COMEDIA("COMEDIA"),
+	DOCUMENTARIO("DOCUMENTARIO"),
+	DRAMA("DRAMA"),
+	EROTICO("EROTICO"),
+	FAROESTE("FAROESTE"),
+	FICCAO("FICCAO"),
+	MUSICAL("MUSICAÇ"),
+	POLICIAL("POLICIAL"),
+	ROMANCE("ROMANCE"),
+	SUSPENSE("SUSPENSE"),
+	TERROR("TERROR"),
+	OUTRO("OUTRO");
+	
+	private final String status;
+	
+	Genero(String status) {
+		this.status = status;
+	}
+	
+	public String getValor() {
+		return this.status;
+	}
+}
