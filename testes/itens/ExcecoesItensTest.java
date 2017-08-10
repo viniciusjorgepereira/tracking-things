336d621599d1b8bbb5597cbdb2361ca14e3a06e6
Vinicius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2017-08-10 09:21:08 -0300

    Adicionado testes de Excecoes dos Itens

---
+++ testes/itens/ExcecoesItensTest.java
@@ -0,0 +1,42 @@
+package itens;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExcecoesItensTest {
+
+	private ExcecoesItens excecoes;
+
+	@Before
+	public void inicializar() {
+		excecoes = new ExcecoesItens();
+	}
+
+	/**
+	 * Lanca uma excecao caso o item procurado nao exista no sistema
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testItemNotFound() {
+		excecoes.itemNotFound();
+	}
+
+	/**
+	 * Testa o status do item, dando uma excecao caso o parametro seja true, do
+	 * contrario, nada ocorre
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testStatusItem() {
+		excecoes.statusItem(true);
+	}
+
+	/**
+	 * Testa a excecao caso o parametro seja invalido
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testAtributoInvalido() {
+		excecoes.atributoInvalido(null);
+	}
+
+}
