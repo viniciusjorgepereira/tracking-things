9851d45ea960c0fd37674bf661951fe82579c6e1
Vinicius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2017-08-22 13:05:53 -0300

    Refatorando

--- src/emprestimo/EmprestimoController.java
+++ src/emprestimo/EmprestimoController.java
@@ -6,34 +6,33 @@ import java.util.List;
 
 public class EmprestimoController {
 	private ArrayList<Emprestimo> emprestimosOrdenados;
-	
+
 	public String listarEmprestimosUsuarioEmprestando(List<Emprestimo> emprestimos) {
 		if (emprestimos.size() > 0) {
 			ordenaEmprestimos(emprestimos);
-			return "Emprestimos: " + listagemEmprestimos();
+			return listagemEmprestimos("Emprestimos: ");
 		}
 		return "Nenhum item emprestado";
+	}
+
+	public String listarEmprestimosUsuarioPegandoEmprestado(List<Emprestimo> emprestimos) {
+		if (emprestimos.size() > 0) {
+			ordenaEmprestimos(emprestimos);
+			return listagemEmprestimos("Emprestimos pegos: ");
+		}
+		return "Nenhum item pego emprestado";
 	}
 
 	private void ordenaEmprestimos(List<Emprestimo> emprestimos) {
 		emprestimosOrdenados = new ArrayList<>(emprestimos);
 		Collections.sort(emprestimosOrdenados, new OrdemEmprestimoNome());
 	}
-	
-	private String listagemEmprestimos() {
-		String saida = "";
-		for(Emprestimo emprestimo: this.emprestimosOrdenados) {
+
+	private String listagemEmprestimos(String msg) {
+		String saida = msg;
+		for (Emprestimo emprestimo : this.emprestimosOrdenados) {
 			saida += emprestimo.toString() + "|";
 		}
 		return saida;
-	}
-	
-	public String listarEmprestimosUsuarioPegandoEmprestado(List<Emprestimo> emprestimos) {
-		ordenaEmprestimos(emprestimos);
-		if (emprestimos.size() > 0) {
-			ordenaEmprestimos(emprestimos);
-			return "Emprestimos pegos: " + listagemEmprestimos();			
-		}
-		return "Nenhum item pego emprestado";
 	}
 }
